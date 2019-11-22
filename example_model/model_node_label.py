89688056060a38b599cf191f94722159ca069f33
sishida21 <ishida.shouichi.57a@st.kyoto-u.ac.jp>
2019-11-22 14:16:09 +0900

    refactor: update example models and configs.

--- example_model/model_node_label.py
+++ example_model/model_node_label.py
@@ -1,59 +1,59 @@
 import tensorflow as tf
-import tensorflow.contrib.keras as K
 import kgcn.layers
 from kgcn.default_model import DefaultModel
 import tensorflow.contrib.keras as K
 
+
 class GCN(DefaultModel):
-    def build_placeholders(self,info,config,batch_size):
+    def build_placeholders(self, info, config, batch_size, **kwargs):
         # input data types (placeholders) of this neural network
-        return self.get_placeholders(info,config,batch_size,
-            ['adjs','nodes','mask','dropout_rate',
-            'node_label','mask_node_label',
-            'enabled_node_nums','is_train','features'])
+        keys = ['adjs', 'nodes', 'mask', 'dropout_rate', 'node_label', 'mask_node_label', 'enabled_node_nums',
+                'is_train', 'features']
+        return self.get_placeholders(info, config, batch_size, keys, **kwargs)
 
-    def build_model(self,placeholders,info,config,batch_size):
-        adj_channel_num=info.adj_channel_num
-        embedding_dim=config["embedding_dim"]
-        in_adjs=placeholders["adjs"]
-        features=placeholders["features"]
-        in_nodes=placeholders["nodes"]
-        labels=placeholders["node_label"]
-        mask_labels=placeholders["mask_node_label"]
-        mask=placeholders["mask"]
-        enabled_node_nums=placeholders["enabled_node_nums"]
-        is_train=placeholders["is_train"]
+    def build_model(self, placeholders, info, config, batch_size, **kwargs):
+        adj_channel_num = info.adj_channel_num
+        embedding_dim = config["embedding_dim"]
+        in_adjs = placeholders["adjs"]
+        features = placeholders["features"]
+        in_nodes = placeholders["nodes"]
+        labels = placeholders["node_label"]
+        mask_labels = placeholders["mask_node_label"]
+        mask = placeholders["mask"]
+        enabled_node_nums = placeholders["enabled_node_nums"]
+        is_train = placeholders["is_train"]
 
-        layer=features
-        input_dim=info.feature_dim
+        layer = features
+        input_dim = info.feature_dim
         if features is None:
-            layer=K.layers.Embedding(info.all_node_num,embedding_dim)(in_nodes)
-            input_dim=embedding_dim
+            layer = K.layers.Embedding(info.all_node_num, embedding_dim)(in_nodes)
+            input_dim = embedding_dim
         # layer: batch_size x graph_node_num x dim
 
-        layer=kgcn.layers.GraphConv(64,adj_channel_num)(layer,adj=in_adjs)
-        layer=kgcn.layers.GraphBatchNormalization()(layer,
-            max_node_num=info.graph_node_num,enabled_node_nums=enabled_node_nums)
-        layer=tf.nn.relu(layer)
+        layer = kgcn.layers.GraphConv(64, adj_channel_num)(layer, adj=in_adjs)
+        layer = kgcn.layers.GraphBatchNormalization()(layer,
+                                                      max_node_num=info.graph_node_num,
+                                                      enabled_node_nums=enabled_node_nums)
+        layer = tf.nn.relu(layer)
 
-        layer=kgcn.layers.GraphConv(64,adj_channel_num)(layer,adj=in_adjs)
-        layer=kgcn.layers.GraphBatchNormalization()(layer,
-            max_node_num=info.graph_node_num,enabled_node_nums=enabled_node_nums)
-        layer=tf.nn.relu(layer)
+        layer = kgcn.layers.GraphConv(64, adj_channel_num)(layer, adj=in_adjs)
+        layer = kgcn.layers.GraphBatchNormalization()(layer,
+                                                      max_node_num=info.graph_node_num,
+                                                      enabled_node_nums=enabled_node_nums)
+        layer = tf.nn.relu(layer)
 
-
-        layer=kgcn.layers.GraphConv(2,adj_channel_num)(layer,adj=in_adjs)
-        prediction=tf.nn.softmax(layer)
+        layer = kgcn.layers.GraphConv(2,adj_channel_num)(layer,adj=in_adjs)
+        prediction = tf.nn.softmax(layer)
         # computing cost and metrics
-        cost=tf.nn.softmax_cross_entropy_with_logits(labels=labels,logits=layer)
-        cost=mask*tf.reduce_mean(cost,axis=1)
-        cost_opt=tf.reduce_mean(cost)
+        cost = tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=layer)
+        cost = mask*tf.reduce_mean(cost, axis=1)
+        cost_opt = tf.reduce_mean(cost)
 
-        metrics={}
-        cost_sum=tf.reduce_sum(cost)
+        metrics = {}
+        cost_sum = tf.reduce_sum(cost)
 
-        pre_count=tf.cast(tf.equal(tf.argmax(prediction,2), tf.argmax(labels,2)),tf.float32)
-        correct_count=mask*tf.reduce_mean(pre_count,axis=1)
-        metrics["correct_count"]=tf.reduce_sum(correct_count)
-        return layer,prediction,cost_opt,cost_sum,metrics
+        pre_count = tf.cast(tf.equal(tf.argmax(prediction, 2), tf.argmax(labels, 2)), tf.float32)
+        correct_count = mask*tf.reduce_mean(pre_count, axis=1)
+        metrics["correct_count"] = tf.reduce_sum(correct_count)
+        return layer, prediction, cost_opt, cost_sum, metrics
 
--- example_model/model_rxn_3layer.py
+++ example_model/model_rxn_3layer.py
@@ -1,81 +1,68 @@
 import tensorflow as tf
-import numpy as np
-import joblib
 import kgcn.legacy.layers
 import tensorflow.contrib.keras as K
+from kgcn.default_model import DefaultModel
 
-def build_placeholders(info,config,batch_size=4,**kwargs):
-    adj_channel_num=info.adj_channel_num
-    placeholders = {
-        'adjs':[[tf.sparse_placeholder(tf.float32,name="adj_"+str(a)+"_"+str(b)) for a in range(adj_channel_num)] for b in range(batch_size)],
-        'nodes': tf.placeholder(tf.int32, shape=(batch_size,info.graph_node_num),name="node"),
-        'labels': tf.placeholder(tf.float32, shape=(batch_size,info.label_dim),name="label"),
-        'mask': tf.placeholder(tf.float32, shape=(batch_size,),name="mask"),
-        'mask_label': tf.placeholder(tf.float32, shape=(batch_size,info.label_dim),name="mask_label"),
-        'dropout_rate': tf.placeholder(tf.float32, name="dropout_rate"),
-        'is_train': tf.placeholder(tf.bool, name="is_train"),
-        'mask_node': tf.placeholder(tf.float32, shape=(batch_size,info.graph_node_num),name="mask_node"),
-        'enabled_node_nums': tf.placeholder(tf.int32, shape=(batch_size,), name="enabled_node_nums"),
-    }
-    if info.feature_enabled:
-        placeholders['features']=tf.placeholder(tf.float32, shape=(batch_size,info.graph_node_num,info.feature_dim),name="feature")
-    else:
-        placeholders['features']=None
-    return  placeholders
 
-def build_model(placeholders,info,config,batch_size=4,**kwargs):
-    adj_channel_num=info.adj_channel_num
-    embedding_dim=config["embedding_dim"]
-    in_adjs=placeholders["adjs"]
-    features=placeholders["features"]
-    in_nodes=placeholders["nodes"]
-    labels=placeholders["labels"]
-    mask=placeholders["mask"]
-    mask_label=placeholders["mask_label"]
-    #dropout_rate=placeholders["dropout_rate"]
-    dropout_rate=0.3
-    is_train=placeholders["is_train"]
-    mask_node=placeholders["mask_node"]
-    enabled_node_nums=placeholders["enabled_node_nums"]
+class GCN(DefaultModel):
+    def build_placeholders(self, info, config, batch_size, **kwargs):
+        # input data types (placeholders) of this neural network
+        keys = ['adjs', 'nodes', 'labels', 'mask', 'mask_label', 'dropout_rate', 'mask_node', 'enabled_node_nums',
+                'is_train', 'features']
+        return self.get_placeholders(info, config, batch_size, keys, **kwargs)
 
-    layer=features
-    input_dim=info.feature_dim
-    with tf.variable_scope("rollout"):
-        if features is None:
-            layer=K.layers.Embedding(info.all_node_num,embedding_dim)(in_nodes)
-            input_dim=embedding_dim
-        # layer: batch_size x graph_node_num x dim
-        layer=kgcn.legacy.layers.GraphConv(128,adj_channel_num)(layer,adj=in_adjs)
-        layer=kgcn.legacy.layers.GraphBatchNormalization()(layer,
-            max_node_num=info.graph_node_num,enabled_node_nums=enabled_node_nums)
-        layer=tf.nn.relu(layer)
-#       layer=K.layers.Dropout(dropout_rate)(layer)
+    def build_model(self, placeholders, info, config, batch_size, **kwargs):
+        adj_channel_num = info.adj_channel_num
+        embedding_dim = config["embedding_dim"]
+        in_adjs = placeholders["adjs"]
+        features = placeholders["features"]
+        in_nodes = placeholders["nodes"]
+        labels = placeholders["labels"]
+        mask = placeholders["mask"]
+        mask_label = placeholders["mask_label"]
+        #dropout_rate=placeholders["dropout_rate"]
+        dropout_rate = 0.3
+        is_train = placeholders["is_train"]
+        mask_node = placeholders["mask_node"]
+        enabled_node_nums = placeholders["enabled_node_nums"]
 
-        layer=kgcn.legacy.layers.GraphConv(128,adj_channel_num)(layer,adj=in_adjs)
-        layer=kgcn.legacy.layers.GraphBatchNormalization()(layer,
-            max_node_num=info.graph_node_num,enabled_node_nums=enabled_node_nums)
-        layer=tf.nn.relu(layer)
-#       layer=K.layers.Dropout(dropout_rate)(layer)
+        layer = features
+        input_dim = info.feature_dim
+        with tf.variable_scope("rollout"):
+            if features is None:
+                layer = K.layers.Embedding(info.all_node_num, embedding_dim)(in_nodes)
+                input_dim = embedding_dim
+            # layer: batch_size x graph_node_num x dim
+            layer = kgcn.legacy.layers.GraphConv(128, adj_channel_num)(layer, adj=in_adjs)
+            layer = kgcn.legacy.layers.GraphBatchNormalization()(layer,
+                                                                 max_node_num=info.graph_node_num,
+                                                                 enabled_node_nums=enabled_node_nums)
+            layer = tf.nn.relu(layer)
 
-        layer=kgcn.legacy.layers.GraphConv(128,adj_channel_num)(layer,adj=in_adjs)
-        layer=kgcn.legacy.layers.GraphBatchNormalization()(layer,
-            max_node_num=info.graph_node_num,enabled_node_nums=enabled_node_nums)
-        layer=tf.nn.relu(layer)
-#       layer=K.layers.Dropout(dropout_rate)(layer)
+            layer = kgcn.legacy.layers.GraphConv(128, adj_channel_num)(layer, adj=in_adjs)
+            layer = kgcn.legacy.layers.GraphBatchNormalization()(layer,
+                                                                 max_node_num=info.graph_node_num,
+                                                                 enabled_node_nums=enabled_node_nums)
+            layer = tf.nn.relu(layer)
 
-        layer=kgcn.legacy.layers.GraphDense(128)(layer)
-        layer=tf.nn.relu(layer)
-#       layer=K.layers.Dropout(dropout_rate)(layer)
+            layer = kgcn.legacy.layers.GraphConv(128, adj_channel_num)(layer, adj=in_adjs)
+            layer = kgcn.legacy.layers.GraphBatchNormalization()(layer,
+                                                                 max_node_num=info.graph_node_num,
+                                                                 enabled_node_nums=enabled_node_nums)
+            layer = tf.nn.relu(layer)
 
-        layer=kgcn.legacy.layers.GraphGather()(layer)
-        layer=K.layers.Dense(info.label_dim)(layer)
-        prediction=tf.nn.softmax(layer,name="output")
-        # computing cost and metrics
-        cost=mask*tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels,logits=layer)
-        cost_opt=tf.reduce_mean(cost)
-        metrics={}
-        cost_sum=tf.reduce_sum(cost)
-        correct_count=mask*tf.cast(tf.equal(tf.argmax(prediction,1), tf.argmax(labels,1)),tf.float32)
-        metrics["correct_count"]=tf.reduce_sum(correct_count)
-    return layer,prediction,cost_opt,cost_sum,metrics
+            layer = kgcn.legacy.layers.GraphDense(128)(layer)
+            layer = tf.nn.relu(layer)
+
+            layer = kgcn.legacy.layers.GraphGather()(layer)
+            layer = K.layers.Dense(info.label_dim)(layer)
+            prediction = tf.nn.softmax(layer, name="output")
+            # computing cost and metrics
+            cost = mask*tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=layer)
+            cost_opt = tf.reduce_mean(cost)
+            metrics = {}
+            cost_sum = tf.reduce_sum(cost)
+            correct_count = mask*tf.cast(tf.equal(tf.argmax(prediction, 1), tf.argmax(labels, 1)), tf.float32)
+            metrics["correct_count"] = tf.reduce_sum(correct_count)
+        return layer, prediction, cost_opt, cost_sum, metrics
 
