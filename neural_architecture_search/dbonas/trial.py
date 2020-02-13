54ce152ad656ce5bd0ebcd848e664d57c720ff3b
0h-n0 <kbu94982@gmail.com>
2020-02-13 14:25:43 +0900

    feat: complete first tiral implementatin. #25

--- neural_architecture_search/dbonas/trial.py
+++ neural_architecture_search/dbonas/trial.py
@@ -1,18 +1,60 @@
-
-class TrialGenerator:
-    def __init__(self):
-        pass
-
-    def register(self, name: str, trial: list):
-        pass
-
-    def __getitem__(self, i):
-        pass
-
-    def __len__(self):
-        pass
-
+import typing
+import random
 
 class Trial:
     def __init__(self):
-        pass
+        self._elements = {}
+
+    def __setattr__(self, name: str, value):
+        super.__setattr__(self, name, value)
+        self._elements[name] = value
+
+    def __str__(self):
+        o = "{"
+        for k, v in self._elements.items():
+            if k == '_elements':
+                continue
+            o += f"{k}: {v}, "
+        o = o[:-2] # remove the last comma.
+        o += "}"
+        return o
+
+
+class TrialGenerator:
+    def __init__(self):
+        self._registered: typing.Dict[str, list] = {}
+        self._registered_length: typing.Dict[str, int] = {}
+        self.trial = Trial()
+        self._len = 1
+
+    def register(self, name: str, trials: typing.List) -> None:
+        assert len(trials) != 0, "can't accept empty trials."
+        if name in self._registered.keys():
+            print(f"{name} is already registered")
+            return
+        self._registered[name] = trials
+        self._registered_length[name] = len(trials)
+        setattr(self.trial, name, None)
+        self._len *= len(trials)
+
+    def __getitem__(self, i: int) -> Trial:
+        indexes: typing.Dict[str, int] = {}
+        for idx, (k, n) in enumerate(self._registered_length.items()):
+            if (idx + 1) == len(self._registered):
+                indexes[k] = i % n
+            else:
+                indexes[k] = i // n
+        for k, n in indexes.items():
+            setattr(self.trial, k, self._registered[k][n])
+        return self.trial
+
+    def __len__(self):
+        if len(self._registered) == 0:
+            return 0
+        return self._len
+
+    def __str__(self):
+        o = ""
+        for k, i in self._registered.values():
+            o += f"{k} : {i}\n"
+        return o
--- neural_architecture_search/test/test_trial.py
+++ neural_architecture_search/test/test_trial.py
@@ -1,9 +1,53 @@
+from itertools import product
+
 from dbonas import TrialGenerator
+
+import pytest
 
 
 def test_trialgenerator_constructor():
-    pass
+    t = TrialGenerator()
+
+def test_trialgenerator_register():
+    t = TrialGenerator()
+    t.register('batch_size', [10, 12])
 
 
-def test_trial_constructor():
-    pass
+@pytest.mark.xfail()
+def test_tiralgenerator_with_empty_trials():
+    """
+    GIVEN: an empty list
+    WHEN: try to register it on TiralGenerator
+    THEN: throw assertion error
+    """
+    t = TrialGenerator()
+    t.register("batch_size", [])
+
+def test_trialgenerator_register_check_len():
+    """
+    GIVEN:
+    WHEN:
+    THEN:
+    """
+    t = TrialGenerator()
+    t.register("batch_size", [1, 2, 3, 4])
+    assert len(t) == 4
+    # same key is registered
+    t.register("batch_size", [1, 2, 3, 4])
+    assert len(t) == 4
+    t.register("lr", [0.1, 0.2, 0.3, 0.4])
+    assert len(t) == 16
+
+
+def test_trial_index_accesses():
+    t = TrialGenerator()
+    lrs = [0.1, 0.2, 0.3, 0.4]
+    t.register("lr", lrs)
+    for i, lr in enumerate(lrs):
+        assert t[i].lr == lr
+    bs = [128, 256, 512, 1024]
+    t.register("batch_size", bs)
+    p = product(lrs, bs)
+    for i, ele in enumerate(p):
+        assert t[i].batch_size == ele[1]
+        assert t[i].lr == ele[0]
