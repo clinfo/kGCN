d7260cca901026710166e5ddcfe6010c78254a3a
kojima <kojima.ryosuke.8e@kyoto-u.ac.jp>
2020-04-14 11:06:12 +0900

    Update run_gen.single.sh

--- sample_chem/generative_model/run_gen.single.sh
+++ sample_chem/generative_model/run_gen.single.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 cd ../../
 
 # run gcn.py train/test
-python gcn_gen.py generate --config sample_chem/generative_model/config_gen.single.json --gpu 0
+kgcn-gen generate --config sample_chem/generative_model/config_gen.single.json --gpu 0
 #
-#python gcn_gen.py generate --config sample_chem/generative_model/config_gen.single_only_link.json --gpu 0
+#kgcn-gen generate --config sample_chem/generative_model/config_gen.single_only_link.json --gpu 0
 
