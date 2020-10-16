a72a023dfb1f594a5bc5cada6c88576ce802c733
0h-n0 <kbu94982@gmail.com>
2020-10-16 09:55:35 +0900

    feat: fl: add chembl dataset.

--- federated_learning/datasets/chembl.py
+++ federated_learning/datasets/chembl.py
@@ -1,48 +1,135 @@
 #!/usr/bin/env
+import collections
 
-#import nest_asyncio
+import pandas as pd
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import rdmolops
+from rdkit.Chem import SaltRemover
 import numpy as np
 import tensorflow as tf
 import tensorflow_federated as tff
+from tensorflow_federated.python.simulation import hdf5_client_data, ClientData
 
-np.random.seed(0)
-print(tff.federated_computation(lambda: 'Hello, World!')())
+from .utils import download as _download
+from .utils import extract_zipfile, create_ids, one_hot, pad_bottom_right_matrix, pad_bottom_matrix
 
 
-# Load simulation data.
-emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data()
+def load_data(max_n_atoms, max_n_types, protein_max_seqlen, n_groups):
+    train_dataset = ChemblDataset(max_n_atoms, max_n_types, protein_max_seqlen, n_groups=n_groups)
+    return train_dataset
 
-print(len(emnist_train.client_ids))
 
-def client_data(n):
-      return source.create_tf_dataset_for_client(source.client_ids[n]).map(
-                lambda e: (tf.reshape(e['pixels'], [-1]), e['label'])
-            ).repeat(10).batch(20)
+class ChemblDataset(ClientData):
+    _urls = {'url': 'https://github.com/clinfo/kGCN/files/5362776/CheMBL_MMP_table_data.zip',
+             'filename': 'CheMBL_MMP_table_data.zip',
+             'csvfilename': 'dataset_benchmark.tsv'}
+    def __init__(self, 
+                 max_n_atoms=150, max_n_types=100, protein_max_seqlen=750, n_groups=2,
+                 savedir='./data', smiles_canonical=True):
+        self.savedir = savedir
+        self.filename = savedir + '/' + self._urls['filename'].replace('.zip', '')
+        _download(self._urls['url'], self._urls['filename'], savedir)
+        extract_zipfile(self.savedir + '/' + self._urls['filename'], savedir)
+        self.max_n_atoms = max_n_atoms
+        self.max_n_types = max_n_types
+        self.protein_max_seqlen = protein_max_seqlen
+        self.n_groups = n_groups
+        self.smiles_canonical = smiles_canonical
+        self._data = self._read_tsv(self.savedir + '/' + self._urls['csvfilename'], skiprows=0)
+        self.salt_remover = SaltRemover.SaltRemover()
+        self._client_ids = sorted(create_ids(n_groups, 'CHEMBL'))
+        self._adj_shape = (max_n_atoms, max_n_atoms)
+        self._feature_shape = (max_n_atoms, max_n_types)
+        # # assign _client_id to data.
+        self.data = {_id: [] for _id in self._client_ids}
+        for data, _id in zip(self._data.itertuples(), np.random.choice(self._client_ids, len(self._data))):
+            self.data[_id].append(self._create_element(data))
+        g = tf.Graph()
+        with g.as_default():
+            tf_dataset = self._create_dataset(self._client_ids[0])
+            self._element_type_structure = tf_dataset.element_spec
 
-# Pick a subset of client devices to participate in training.
-train_data = [client_data(n) for n in range(3)]
+    def _create_element(self, data):
+        label = np.array(data[1], dtype=np.int32)
+        mol = Chem.MolFromSmiles(data[4])
+        if self.smiles_canonical:
+            smiles = Chem.MolToSmiles(mol, canonical=True)
+            mol = Chem.MolFromSmiles(smiles)
+        features = pad_bottom_matrix(self._create_mol_feature(mol), self.max_n_atoms)
+        adjs = pad_bottom_right_matrix(rdmolops.GetAdjacencyMatrix(mol), self.max_n_atoms)
+        res = self.salt_remover.StripMol(mol, dontRemoveEverything=True)
+        protein_seq = self._create_one_hot_protein_seq(data[5])
+        return {'label': label,
+                'adjs': adjs,
+                'features': features,
+                'protein_seq': protein_seq}
 
-# Grab a single batch of data so that TFF knows what data looks like.
-sample_batch = tf.nest.map_structure(
-    lambda x: x.numpy(), iter(train_data[0]).next())
+    def _create_one_hot_protein_seq(self, protein_seq: str):
+        one_letter_aa = 'XACDEFGHIKLMNPQRSTVWY'
+        vec = []
+        for idx in range(self.protein_max_seqlen):
+            if len(protein_seq) > idx:
+                c = protein_seq[idx]
+                vec.append(one_letter_aa.index(c))
+            else:
+                vec.append(one_letter_aa.index('X'))
+        return vec
 
-# Wrap a Keras model for use with TFF.
-def model_fn():
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(10, tf.nn.softmax, input_shape=(784,),
-                              kernel_initializer='zeros')
-    ])
-    return tff.learning.from_keras_model(
-        model,
-        dummy_batch=sample_batch,
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+    def _create_mol_feature(self, mol):
+        mol_features = np.array([m.GetAtomicNum() for m in mol.GetAtoms()])
+        mol_features = one_hot(mol_features, self.max_n_types).astype(np.int32)
+        return mol_features
 
-      # Simulate a few rounds of training with the selected client devices.
-trainer = tff.learning.build_federated_averaging_process(
-    model_fn,
-    client_optimizer_fn=lambda: tf.keras.optimizers.SGD(0.1))
-state = trainer.initialize()
-for _ in range(5):
-    state, metrics = trainer.next(state, train_data)
-    print (metrics.loss)
+    def _create_dataset(self, client_id):
+        _data = collections.OrderedDict({key: [] for key in sorted(self.data[client_id][0].keys())})
+        for idx, mol in enumerate(self.data[client_id]):
+            _data['label'].append(mol['label'])
+            _data['adjs'].append(mol['adjs'])
+            _data['features'].append(mol['features'])
+            _data['protein_seq'].append(mol['protein_seq'])
+        _data = collections.OrderedDict((name, np.array(ds))
+                                        for name, ds in sorted(_data.items()))
+        return tf.data.Dataset.from_tensors(_data)
+
+    @property
+    def adj_shape(self):
+        return self._adj_shape
+
+    @property
+    def feature_shape(self):
+        return self._feature_shape
+
+    @property
+    def client_ids(self):
+        return self._client_ids
+    
+    @property
+    def element_type_structure(self):
+        return self._element_type_structure
+    
+    @property
+    def dataset_computation(self):
+        raise NotImplementedError("tox21:dataset_computation")
+
+    def create_tf_dataset_for_client(self, client_id):
+        if client_id not in self.client_ids:
+            raise ValueError(
+                "ID [{i}] is not a client in this ClientData. See "
+                "property `client_ids` for the list of valid ids.".format(
+                    i=client_id))
+        tf_dataset = self._create_dataset(client_id)
+        # tensor_utils.check_nested_equal(tf_dataset.element_spec,
+        #                                 self._element_type_structure)
+        return tf_dataset
+
+    def _read_tsv(self, filename, skiprows=None):
+        df = pd.read_csv(filename, delimiter='\t')
+        return df
+
+    def __len__(self):
+        return self._len
+
+
+if __name__ == "__main__":
+    c = ChemblDataset()
