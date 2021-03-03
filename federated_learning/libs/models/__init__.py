8e40845580c259645a73f44eda713908f88fdefe
0h-n0 <kbu94982@gmail.com>
2021-03-03 23:28:38 +0900

    feat: add scripts.

--- federated_learning/libs/models/__init__.py
+++ federated_learning/libs/models/__init__.py
@@ -20,7 +20,6 @@ def build_model_gin(max_n_atoms, max_n_types, num_classes):
         logits = tf.keras.layers.Dense(num_classes, name="dense", activation='softmax')(h)        
     return tf.keras.Model(inputs=[input_adjs, input_features], outputs=logits)
 
-
 def build_model_gcn(max_n_atoms, max_n_types, num_classes):
     input_adjs = tf.keras.Input(
         shape=(1, max_n_atoms, max_n_atoms), name='adjs', sparse=False)
@@ -37,3 +36,76 @@ def build_model_gcn(max_n_atoms, max_n_types, num_classes):
     else:
         logits = tf.keras.layers.Dense(num_classes, name="dense", activation='softmax')(h)        
     return tf.keras.Model(inputs=[input_adjs, input_features], outputs=logits)
+
+def build_model_discriptor_gcn(max_n_atoms, max_n_types, num_classes, num_descriptors_features):
+    input_adjs = tf.keras.Input(
+        shape=(1, max_n_atoms, max_n_atoms), name='adjs', sparse=False)
+    input_features = tf.keras.Input(
+        shape=(max_n_atoms, max_n_types), name='features')
+    input_descriptors = tf.keras.Input(
+        shape=(num_descriptors_features), name='descriptor')
+    # for graph
+    h = layers.GraphConvFL(64, 1)(input_features, input_adjs)
+    h = tf.keras.layers.ReLU()(h)
+    h = tf.keras.layers.Dropout(0.5)(h)    
+    h = layers.GraphConvFL(64, 1)(h, input_adjs)
+    h = tf.keras.layers.ReLU()(h)
+    h = tf.keras.layers.Dropout(0.5)(h)        
+    h = layers.GraphGather()(h)
+
+    # for descriptors
+    hd = tf.keras.layers.Dense(128, activation='relu')(input_descriptors)
+    hd = tf.keras.layers.Dropout(0.5)(hd)    
+    hd = tf.keras.layers.Dense(64, activation='relu')(hd)
+    hd = tf.keras.layers.Dropout(0.5)(hd)        
+    h = tf.keras.layers.Concatenate()([h, hd])
+    
+    if num_classes == 1:
+        logits = tf.keras.layers.Dense(num_classes)(h)
+    else:
+        logits = tf.keras.layers.Dense(num_classes, activation='softmax')(h)        
+    return tf.keras.Model(inputs=[input_adjs, input_features, input_descriptors], outputs=logits)
+
+def build_model_gat(max_n_atoms, max_n_types, num_classes, num_heads=3):
+    input_adjs = tf.keras.Input(
+        shape=(1, max_n_atoms, max_n_atoms), name='adjs', sparse=False)
+    input_features = tf.keras.Input(
+        shape=(max_n_atoms, max_n_types), name='features')
+    # for graph
+    h = layers.GATFL(32, num_heads, 1)(input_features, input_adjs)
+    h = layers.GATFL(32, num_heads, 1)(h, input_adjs)
+    h = layers.GraphGather()(h)
+    if num_classes == 1:
+        logits = tf.keras.layers.Dense(num_classes, name="dense")(h)
+    else:
+        logits = tf.keras.layers.Dense(num_classes, name="dense", activation='softmax')(h)        
+    return tf.keras.Model(inputs=[input_adjs, input_features], outputs=logits)
+
+def build_multimodel_gcn(max_n_atoms, max_n_types, num_classes, protein_max_seqlen, length_one_letter_aa, output_bias=None):
+    if output_bias is not None:
+        output_bias = tf.keras.initializers.Constant(output_bias)
+    input_adjs = tf.keras.Input(
+        shape=(1, max_n_atoms, max_n_atoms), name='adjs', sparse=False)
+    input_features = tf.keras.Input(
+        shape=(max_n_atoms, max_n_types), name='features')
+    input_protein_seq = tf.keras.Input(shape=(protein_max_seqlen), name='protein_seq')    
+    # for graph
+    #h = layers.GraphConvFL(64, 1)(input_features, input_adjs)
+    h = layers.GINFL(128, 1)(input_features, input_adjs)
+    h = tf.keras.layers.ReLU()(h)
+    #h = layers.GraphConvFL(64, 1)(h, input_adjs)
+    h = layers.GINFL(128, 1)(h, input_adjs)
+    h = tf.keras.layers.ReLU()(h)
+    h = layers.GraphGather()(h)
+
+    # for protein sequence
+    h_seq = tf.keras.layers.Embedding(length_one_letter_aa, 128, input_length=protein_max_seqlen)(input_protein_seq)
+    stride = 4
+    #h_seq = tf.keras.layers.GlobalAvePool1D()(h_seq)
+    h_seq = tf.keras.layers.Dense(64, activation='relu')(h_seq)
+    h_seq = tf.keras.layers.Dropout(0.5)(h_seq)
+    h_seq = tf.keras.layers.GlobalAveragePooling1D()(h_seq)
+    h = tf.keras.layers.Concatenate()([h, h_seq])
+    h = tf.keras.layers.Dropout(0.5)(h)    
+    logits = tf.keras.layers.Dense(1, name="logits", activation='sigmoid', bias_initializer=output_bias)(h)
+    return tf.keras.Model(inputs=[input_adjs, input_features, input_protein_seq], outputs=logits)
